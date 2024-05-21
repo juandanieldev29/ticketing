02f86edff1310f01858b08a49d546d5c83ddfb31
Juan Daniel <juandanieldev29@gmail.com>
2024-05-20 20:01:06 -0600

    adding signup and kubernetes secrets to sign a JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -18,9 +20,21 @@ router.post('/api/users/signup', [
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array());
   }
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
-  // res.send({});
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError('Email in use');
+  }
+  const user = User.build({ email, password });
+  await user.save();
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, process.env.JWT_KEY!);
+  req.session = {
+    jwt: userJwt
+  };
+  res.status(201).send(user);
 });
 
 export { router as signupRouter };
